f8175b0ac895b2231488cf98d7fc8f3aa8e3da06
ryan <ryanwkoch@gmail.com>
2017-08-03 22:08:31 -0400

    heroku set up
    heroku set up and ready to go

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const mongooseBaseName = 'express-api-template'
+const mongooseBaseName = 'group-project-backend'
 
 const environment = process.env.NODE_ENV || 'development'
 const database = {
